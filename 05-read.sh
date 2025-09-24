190c5c46b674a44b426635675b3fcf1cd6f4a327
khajit <ssvvkhajit@gmail.com>
2025-09-25 01:44:34 +0530

    msg

--- 05-read.sh
+++ 05-read.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Please enter pin"
-read Pin_number #here Pin_number will be the variable
+read -s Pin_number #here Pin_number will be the variable
 
 echo "$Pin_number is the pin" 
