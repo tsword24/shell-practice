0c0579b5fa38ef53f36e6ab15947a683c199fb21
khajit <ssvvkhajit@gmail.com>
2025-09-25 22:51:00 +0530

    special variables

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -4,3 +4,9 @@ echo "all the variables passed are: $@"
 echo "all the variables passed are: $*"
 
 echo "script name:$0"
+
+echo "present working directory $PWD"
+
+echo "home directory of the user:$HOME"
+
+echo "user :$USER"
