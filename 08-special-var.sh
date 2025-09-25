69fb5a02b396ec8e558475ea2f8006878b2bf067
khajit <ssvvkhajit@gmail.com>
2025-09-25 22:57:14 +0530

    special variables

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -10,3 +10,9 @@ echo "present working directory $PWD"
 echo "home directory of the user:$HOME"
 
 echo "user :$USER"
+
+echo "PID of the script:$$"
+
+sleep 50 & #this to execute for getting pid of this commnand
+
+echo "PID of the last executed command: $!"
