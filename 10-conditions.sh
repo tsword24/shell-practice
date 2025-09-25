e94ec12c815cb0340dc9bbc4574cfa8e4d420f5d
khajit <ssvvkhajit@gmail.com>
2025-09-26 00:03:39 +0530

    conditons

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -6,3 +6,11 @@ if [ $NUM -lt 10 ];then
 else 
     echo "num is greater than 10"
 fi
+
+if [ $NUM -lt 10];then
+    echo "num is less than 10"
+elif [$NUM -eq 10 ];then
+    echo "num is equal to 10"
+else
+    echo "num is greater than 20"
+fi
