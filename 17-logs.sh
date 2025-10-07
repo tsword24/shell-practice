0ba97b93f6bd661c2e7b6f986a429430ea56768b
khajit <ssvvkhajit@gmail.com>
2025-10-07 23:00:46 +0530

    colors and validation

---
+++ 17-logs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-practice"
+SCRIPT_NAME="$( echo $0 | cut -d "." -f1)"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script Started at $(date)" 
+
+if [ $USERID -ne 0 ]; then 
+    echo "Give root privilages"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];then
+        echo -e "Install is $G success$N" 
+    else
+        echo -e "$R Failure$N"
+    fi
+}
+
+dnf install mysql -y &>>$LOG_FILE
+if [ $? -ne 0 ];then 
+    echo "Installing my sql"
+    VALIDATE $? "MYSQL"
+else
+    echo -e "Already installed ... $Y Skipping $N"
+fi
+
+dnf install nginx -y &>>$LOG_FILE
+if [ $? -ne 0 ];then 
+    echo "Installing nginx"
+    VALIDATE $? "nginx"
+else
+    echo -e "Already installed ... $Y Skipping $N"
+fi
