30f82d6082ef9300516dcd5dcad37dc793b905b8
khajit <ssvvkhajit@gmail.com>
2025-10-07 21:10:30 +0530

    tests

---
+++ 14-functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USERIS=$(id -u)
+
+if [ $USERIS -ne 0 ];then
+    echo "give root privilages"
+else
+    echo "continue"
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "installation of $2 is unsuccessful"
+    else
+        echo "installation of $2 is successful"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "Mysql"
+
+dnf install nginx -y
+VALIDATE $? "Nginx
