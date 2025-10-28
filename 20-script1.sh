6590f810fed72a55419d7ff88b499ac6f47ac359
khajit <ssvvkhajit@gmail.com>
2025-10-28 17:21:09 +0530

    common file

--- 20-script1.sh
+++ 20-script1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+NAME=India
 echo "My country: $NAME"
 echo "PID of SCRIPT-1:$$"
+
+sh 21-script2.sh
--- 21-script2.sh
+++ 21-script2.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-NAME=India
 echo "MY COuntry:$NAME"
 echo "PID of Script: $$"
 
-sh 21-script2.sh
