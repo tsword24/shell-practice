97bd274a9b69336f07f32f060b287d28873858dc
khajit <ssvvkhajit@gmail.com>
2025-10-30 00:29:27 +0530

    backup files

--- 23-backup.sh
+++ 23-backup.sh
@@ -44,9 +44,9 @@ if [ ! -d $SOURCE_DIR ];then
 fi
 
 
-FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime -14)
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 
-if [ ! -z $FILES ];then
+if [ ! -z "${FILES}" ];then
     echo "Files found: $FILES"
     TIMESTAMP=$(date +%F-%H-%M)
     ZIP_FILE_NAME="$DESTINATION_DIR/app-logs-$TIMESTAMP.zip"
