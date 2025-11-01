41b49755720d25f2b6b33b05f90c628d7983edd3
khajit <ssvvkhajit@gmail.com>
2025-11-01 17:07:41 +0530

    This is disk usage

---
+++ 25-cpuusage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT|grep -v Filesystem)
+DISK_THRESHOLD=2
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=""
+while IFS= read -r line
+do
+    USAGE=$(echo $line|awk '{print $6}'|cut -d "%" -f1)
+    PARTITION=$(echo $line|awk '{print $7}')
+    if [ $USAGE -ge $DISK_THRESHOLD ];then
+        MESSAGE+="High Disk usage alert $PARTITION:$USAGE% \n"
+    fi
+done <<< $DISK_USAGE
+
+echo "Body of the message:$MESSAGE"
