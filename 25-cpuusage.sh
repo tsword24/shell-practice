eb379e7c84a2479866c1fe7229fd9f0dc65e9c6f
khajit <ssvvkhajit@gmail.com>
2025-11-01 17:08:54 +0530

    This is disk usage

--- 25-cpuusage.sh
+++ 25-cpuusage.sh
@@ -9,7 +9,7 @@ do
     USAGE=$(echo $line|awk '{print $6}'|cut -d "%" -f1)
     PARTITION=$(echo $line|awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ];then
-        MESSAGE+="High Disk usage alert $PARTITION:$USAGE% \n"
+        MESSAGE+="High Disk usage alert $PARTITION:$USAGE% /n"
     fi
 done <<< $DISK_USAGE
 
