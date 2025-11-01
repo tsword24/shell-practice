e37dafaf62c2984f0735827ee5984ed4e21320a4
khajit <ssvvkhajit@gmail.com>
2025-11-01 17:48:58 +0530

    This is disk usage

--- 25-cpuusage.sh
+++ 25-cpuusage.sh
@@ -2,7 +2,7 @@
 
 DISK_USAGE=$(df -hT|grep -v Filesystem)
 DISK_THRESHOLD=2
-IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 MESSAGE=""
 while IFS= read -r line
 do
@@ -14,3 +14,11 @@ do
 done <<< $DISK_USAGE
 
 echo -e "Body of the message:$MESSAGE"
+
+sh mail.sh "saiveer2003@gmail.com" "High disk usage alert" "Disk Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
+# TO_ADDRESS=$1
+# SUBJECT=$2
+# ALERT_TYPE=$3
+# MESSAGE=$4
+# IP_ADDRESS=$5
+# TO_TEAM=$6
---
+++ mail.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TO_ADDRESS=$1
+SUBJECT=$2
+ALERT_TYPE=$3
+MESSAGE=$4
+IP_ADDRESS=$5
+TO_TEAM=$6
+
+FINAL_BODY=$(sed -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$MESSAGE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/TO_TEAM/$TO_TEAM/g" -e "s/")
+
+{
+echo "To: $TO_ADDRESSS"
+echo "Subject: $SUBJECT"
+echo "Content-Type: text/html"
+echo ""
+echo "$FINAL_BODY"
+} | msmtp "$TO_ADDRESS"
